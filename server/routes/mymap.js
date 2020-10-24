fe09a7445d7fb5780525bb1a0ed58dab16020364
107081029 <renhua61yang@gmail.com>
2020-10-24 11:21:23 +0800

    add map

---
+++ server/routes/mymap.js
@@ -0,0 +1,46 @@
+const db = require('../db');
+const express = require('express');
+const router = express.Router();
+
+// export our router to be mounted by the parent application
+module.exports = router;
+
+router.post('/add', async (req, res) => {
+  const { name, place, time } = req.body;
+  try { 
+    const { rows } = await db.query('INSERT INTO campaign(name, place, time) VALUES($1, $2, $3) RETURNING *', [name, place, time]);
+    res.send(rows[0]);
+  } catch (error) {
+    console.error(error.stack);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const campaignId = req.params.id;
+  try {
+    const { rows } = await db.query('SELECT DISTINCT campaign.id, campaign.name as campaignName, member.name, member.is_join FROM campaign LEFT JOIN member ON campaign_id = $1 WHERE campaign.id = $1', [campaignId]);
+    const members = rows.filter((row) => {
+      if (row.name) return row
+    })
+    res.send({
+      campaignName: rows.length > 0 ? rows[0].campaignname : null,
+      members
+    });
+  } catch (error) {
+    console.error(error.stack);
+  }
+});
+
+router.post('/:id/join', async (req, res) => {
+  const { uid, name, isJoin } = req.body;
+  const campaignId = req.params.id;
+  try {
+    const { rows } = await db.query('SELECT * FROM member WHERE uid = $1 AND campaign_id = $2', [uid, campaignId]);
+    const { rows: result } = rows.length > 0
+      ? await db.query('UPDATE member SET is_join=$1 WHERE uid = $2 AND campaign_id = $3 RETURNING *', [isJoin, uid, campaignId])
+      : await db.query('INSERT INTO member(uid, name, is_join, campaign_id) VALUES($1, $2, $3, $4) RETURNING *', [uid, name, isJoin, campaignId]);
+    res.send(result[0]);
+  } catch (error) {
+    console.error(error.stack);
+  }
+});
